776c6e5ff3e303e09686f3590a30ace4c869169d
Pavel Filatov <pavel-filatov@yandex-team.ru>
2022-02-15 12:03:31 +0300

    add button tooltips

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -10,26 +10,30 @@ interface Props {
   disabled?: boolean;
   type?: 'submit' | 'reset' | 'button' | undefined;
   onClick?: () => void;
+  tooltipId?: string;
+  tooltipText?: string | null;
 }
 
 const Button: React.FC<Props> = forwardRef<HTMLButtonElement, Props>(
-  ({ children, variant = 'primary', onClick, className, disabled, type }, ref) => {
+  ({ children, variant = 'primary', onClick, className, disabled, type, tooltipId, tooltipText }, ref) => {
     return (
-      <button
-        className={classnames(className, {
-          [styles.buttonPrimary]: variant === 'primary',
-          [styles.buttonSecondary]: variant === 'secondary',
-          [styles.buttonGhost]: variant === 'ghost',
-          [styles.buttonWarning]: variant === 'warning',
-          [styles.disabled]: disabled,
-        })}
-        ref={ref}
-        disabled={disabled}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
+      <span data-tip={tooltipText} data-for={tooltipId}>
+        <button
+          className={classnames(className, {
+            [styles.buttonPrimary]: variant === 'primary',
+            [styles.buttonSecondary]: variant === 'secondary',
+            [styles.buttonGhost]: variant === 'ghost',
+            [styles.buttonWarning]: variant === 'warning',
+            [styles.disabled]: disabled,
+          })}
+          ref={ref}
+          disabled={disabled}
+          onClick={onClick}
+          type={type}
+        >
+          {children}
+        </button>
+      </span>
     );
   }
 );
