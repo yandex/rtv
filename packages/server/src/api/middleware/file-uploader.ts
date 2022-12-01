11d0b81c6138cdeafd71454be21f533f1fb9c9e1
Pavel Filatov <pavel-filatov@yandex-team.ru>
2022-12-01 19:36:44 +0300

    save uploads to rtv-data

--- packages/server/src/api/middleware/file-uploader.ts
+++ packages/server/src/api/middleware/file-uploader.ts
@@ -7,7 +7,7 @@ import { v4 as uuid } from 'uuid';
 import { values as config } from '../../config';
 
 const storage = multer.diskStorage({
-  destination: path.join(config.workDirPath, 'uploads'),
+  destination: path.join(config.rtvDataPath, 'uploads'),
   filename: function (_req, file, cb) {
     const pathname = uuid().replace(/-/g, ''); //Replace "-" for Orsay, seems it is not supported in download links
     cb(null, `${pathname}${path.extname(file.originalname)}`);
